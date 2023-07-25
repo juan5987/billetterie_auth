17c3901d242055df7c71c0eb19904b7d947bdb28
Juan5987 <juan.machado.5987@gmail.com>
2023-07-25 11:49:42 +0200

    added validationResult to handle validation errors

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response} from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
@@ -13,8 +13,16 @@ router.post('/api/users/signup', [
     .withMessage('Le mot de passe doit contenir au moins 8 caractères')
 ], 
 (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).send(errors.array());
+    }
+
     const { email, password } = req.body;
 
+    console.log("Creating user...");
+    res.send({});
     
 });
 
