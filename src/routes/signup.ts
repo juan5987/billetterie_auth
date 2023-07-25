8acb80a53611881bdde8cd0687c4835a9e628b48
Juan5987 <juan.machado.5987@gmail.com>
2023-07-25 11:46:07 +0200

    added signup validation

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,8 +1,18 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
+router.post('/api/users/signup', [
+    body('email')
+    .isEmail()
+    .withMessage('Email invalide'),
+    body('password')
+    .trim()
+    .isLength({ min: 8})
+    .withMessage('Le mot de passe doit contenir au moins 8 caractères')
+], 
+(req: Request, res: Response) => {
     const { email, password } = req.body;
 
     
