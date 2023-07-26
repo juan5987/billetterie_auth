8a741c75c4518f98cbdf40619c27b4a3842b9c6a
Juan5987 <juan.machado.5987@gmail.com>
2023-07-26 12:05:48 +0200

    added signin logic

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, {Request, Response} from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
@@ -14,7 +18,35 @@ router.post('/api/users/signin', [
         .withMessage('Vous devez entrer un mot de passe')
 ],
 validateRequest,
-    (req:Request, res: Response) => {
+    async (req:Request, res: Response) => {
+        const { email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if(!existingUser) {
+            throw new BadRequestError('Identifiants invalides');
+        }
+
+        const passwordsMatch = await Password.compare(existingUser.password, password);
+
+        if(!passwordsMatch) {
+            throw new BadRequestError('Identifiants invalides');
+        } 
+
+        // Generate JWT
+        const userJwt = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email
+        },
+        process.env.JWT_KEY!
+        );
+    
+        // Stocker le JWT dans la session
+        req.session = {
+            jwt: userJwt
+        };
+    
+        res.status(200).send(existingUser);
+
 
     });
 
