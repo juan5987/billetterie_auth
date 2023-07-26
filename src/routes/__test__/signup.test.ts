493b50ed908e34238f45fc67e393b468ee0e0839
Juan5987 <juan.machado.5987@gmail.com>
2023-07-26 16:13:00 +0200

    added test 'disallows duplicate emails'

--- src/routes/__test__/signup.test.ts
+++ src/routes/__test__/signup.test.ts
@@ -14,36 +14,54 @@ it('returns a 201 on successful signup', async () => {
 
 it('returns a 400 with an invalid email', async () => {
     return request(app)
-    .post('/api/users/signup')
-    .send({
-        email: 'test@test',
-        password: 'password'
-    })
-    .expect(400);
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test',
+            password: 'password'
+        })
+        .expect(400);
 })
 
 it('returns a 400 with an invalid password', async () => {
     return request(app)
-    .post('/api/users/signup')
-    .send({
-        email: 'test@test',
-        password: 'pa'
-    })
-    .expect(400);
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test',
+            password: 'pa'
+        })
+        .expect(400);
 })
 
 it('returns a 400 with missing email or password', async () => {
     await request(app)
-    .post('/api/users/signup')
-    .send({
-        email: 'test@test.com'
-    })
-    .expect(400);
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com'
+        })
+        .expect(400);
 
     await request(app)
-    .post('/api/users/signup')
-    .send({
-        password: 'password'
-    })
-    .expect(400);
+        .post('/api/users/signup')
+        .send({
+            password: 'password'
+        })
+        .expect(400);
 });
+
+it('disallows duplicate emails', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(400);
+})
