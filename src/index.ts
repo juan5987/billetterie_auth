e92ad4c49136b26ca31b43ff2ed9c79dc1320960
Juan5987 <juan.machado.5987@gmail.com>
2023-07-26 10:43:34 +0200

    added cookie-session

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession  from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -11,7 +12,17 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+// Pour que express fasse confiance à nginx
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+    cookieSession({
+        // pas besoin de chiffré car le JWT sera chiffré
+        signed: false,
+        // https uniquement
+        secure: true
+    })
+)
 
 app.use(currentUserRouter);
 app.use(signinRouter);
