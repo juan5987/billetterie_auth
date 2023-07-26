76ff9de2312ed3aa9e84d0930381659707d0f6ce
Juan5987 <juan.machado.5987@gmail.com>
2023-07-26 14:28:37 +0200

    refacto: created app.ts to separate express app from other app config logic

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession  from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+// Pour que express fasse confiance à nginx et qu'il sache qu'il est derrière un proxy
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        // pas besoin de chiffré car le JWT sera chiffré
+        signed: false,
+        // https uniquement
+        secure: true
+    })
+)
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession  from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-// Pour que express fasse confiance à nginx et qu'il sache qu'il est derrière un proxy
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-    cookieSession({
-        // pas besoin de chiffré car le JWT sera chiffré
-        signed: false,
-        // https uniquement
-        secure: true
-    })
-)
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async (req, res) => {
-    throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
     try{
