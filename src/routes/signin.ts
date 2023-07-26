24c7800246afe65915272d19033c1b46183a46f8
Juan5987 <juan.machado.5987@gmail.com>
2023-07-26 11:24:24 +0200

    added request validation and error handling

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('Hi there!');
-});
+router.post('/api/users/signin', [
+    body('email')
+        .isEmail()
+        .withMessage('Email invalide'),
+    body('password')
+        .trim()
+        .notEmpty()
+        .withMessage('Vous devez entrer un mot de passe')
+],
+    (req:Request, res: Response) => {
+        const errors = validationResult(req);
+
+
+        if(!errors.isEmpty()){
+            throw new RequestValidationError(errors.array());
+        }
+    });
 
 export { router as signinRouter };
